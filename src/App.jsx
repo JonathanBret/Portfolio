eebee65fc128ec498459fd72e09ce6eec998ec01
JonathanBret <bret.jonathan@outlook.fr>
2025-03-11 15:38:11 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Nav from "./components/nav";
+import Header from "./components/header";
+import AccessPage from "./components/accesPage.jsx"; 
+
+function App() {
+  return (
+    <div className="container">
+      <AccessPage />
+    </div>
+  );
+}
+
+export default App;
