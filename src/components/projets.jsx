2830f480e261e5c9db9f27224aa5ff5112bdea82
JonathanBret <bret.jonathan@outlook.fr>
2025-03-15 00:22:25 +0100

    feat : scroll animation on projets component

--- src/components/projets.jsx
+++ src/components/projets.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react';
 import '../style/projets.css';
 import kasa from '../assets/Kasa3.png';
 import nina from '../assets/nina.png'
@@ -5,15 +6,40 @@ import sophie from '../assets/sophie.png'
 import booki from '../assets/booki.png'
 
 const Projets = () => {
+
+    const [visible, setVisible] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        function handleScroll() {
+            if (ref.current) {
+                const projetsTop = ref.current.getBoundingClientRect().top;
+
+                if (projetsTop < window.innerHeight * 0.75) {
+                    setVisible(true);
+                }
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
 return (
     <div className="projets-margin">
-        <div id="projets-section" className="projets-container">
+        <div 
+        id="projets-section" 
+        className={`projets-container ${visible ? 'visible' : 'hidden'}`}
+            ref={ref}
+            >
             <div className="projets-text">
                 <h1 className="projets-title">La création commence par la vision</h1>
                 <p>Découvrez mes projets réalisés, où chaque ligne de code reflète ma passion pour l'innovation et mon engagement à offrir des solutions créatives et fonctionnelles.</p>
             </div>
-            <div className="all-projets">
-                <div className="projets-card">
+            <div className={`all-projets ${visible ? 'visible' : 'hidden'}`}
+                ref={ref}>
+                <div className="projets-card"
+                >
                     <div id="kasa" className='projets-image'>
                     <img src={kasa} alt=""></img>
                     </div>
