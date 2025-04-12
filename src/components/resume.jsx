bf42f443753a87e26d2748868e3af65870200fc7
JonathanBret <bret.jonathan@outlook.fr>
2025-04-12 02:43:58 +0200

    add resume

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,21 +1,22 @@
-import '../style/resume.css'
+import '../style/resume.css';
+import CV from '../assets/CV.pdf';
 
-const Resume =() => {
-    return(
+const Resume = () => {
+    return (
         <div className="resume-margin">
             <div id="resume-section" className="resume-container">
                 <div className="resume-title">
                     <h2>Télécharger directement mon CV ici 👉</h2>
                     <p>Vous voulez en savoir plus sur Jonathan Bret ?</p>
                 </div>
-                    <div className='resume-button'>
-                        <a href="">
-                            <button className="resume-download">Télécharger</button>
-                        </a>
-                    </div>
+                <div className="resume-button">
+                    <a href={CV} download>
+                        <button className="resume-download">Télécharger</button>
+                    </a>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Resume
+export default Resume;
